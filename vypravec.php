9caf69dd86c47067b96363078780731fc9664b6d
VitKolos <vit.kolos@gmail.com>
2016-10-23 11:30:53 +0200

    Přidány některé užitečné funkce.

--- vypravec.php
+++ vypravec.php
@@ -90,7 +90,7 @@ if (!isset($nazev)) {
 						<tr><td><div id="hraci" onclick="upravit()"><div style="text-align:center;"><svg width="40px" height="40px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" class="uil-default"><rect x="0" y="0" width="100" height="100" fill="none" class="bk"></rect><rect  x="46.5" y="40" width="7" height="20" rx="5" ry="5" fill="#00b2ff" transform="rotate(0 50 50) translate(0 -30)">  <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0s" repeatCount="indefinite"/></rect><rect  x="46.5" y="40" width="7" height="20" rx="5" ry="5" fill="#00b2ff" transform="rotate(30 50 50) translate(0 -30)">  <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.08333333333333333s" repeatCount="indefinite"/></rect><rect  x="46.5" y="40" width="7" height="20" rx="5" ry="5" fill="#00b2ff" transform="rotate(60 50 50) translate(0 -30)">  <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.16666666666666666s" repeatCount="indefinite"/></rect><rect  x="46.5" y="40" width="7" height="20" rx="5" ry="5" fill="#00b2ff" transform="rotate(90 50 50) translate(0 -30)">  <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.25s" repeatCount="indefinite"/></rect><rect  x="46.5" y="40" width="7" height="20" rx="5" ry="5" fill="#00b2ff" transform="rotate(120 50 50) translate(0 -30)">  <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.3333333333333333s" repeatCount="indefinite"/></rect><rect  x="46.5" y="40" width="7" height="20" rx="5" ry="5" fill="#00b2ff" transform="rotate(150 50 50) translate(0 -30)">  <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.4166666666666667s" repeatCount="indefinite"/></rect><rect  x="46.5" y="40" width="7" height="20" rx="5" ry="5" fill="#00b2ff" transform="rotate(180 50 50) translate(0 -30)">  <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.5s" repeatCount="indefinite"/></rect><rect  x="46.5" y="40" width="7" height="20" rx="5" ry="5" fill="#00b2ff" transform="rotate(210 50 50) translate(0 -30)">  <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.5833333333333334s" repeatCount="indefinite"/></rect><rect  x="46.5" y="40" width="7" height="20" rx="5" ry="5" fill="#00b2ff" transform="rotate(240 50 50) translate(0 -30)">  <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.6666666666666666s" repeatCount="indefinite"/></rect><rect  x="46.5" y="40" width="7" height="20" rx="5" ry="5" fill="#00b2ff" transform="rotate(270 50 50) translate(0 -30)">  <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.75s" repeatCount="indefinite"/></rect><rect  x="46.5" y="40" width="7" height="20" rx="5" ry="5" fill="#00b2ff" transform="rotate(300 50 50) translate(0 -30)">  <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.8333333333333334s" repeatCount="indefinite"/></rect><rect  x="46.5" y="40" width="7" height="20" rx="5" ry="5" fill="#00b2ff" transform="rotate(330 50 50) translate(0 -30)">  <animate attributeName="opacity" from="1" to="0" dur="1s" begin="0.9166666666666666s" repeatCount="indefinite"/></rect></svg></div></div></td></tr>
 						<tr><td style="border-bottom: none;"><div id="chat" style="max-height:40vh; overflow:scroll; overflow-x: hidden;"><div style="text-align:center;"></div></div></td></tr>
 						<tr><td style="border-top: none; text-align:center;"><div id="rolovat" onclick="roluj()" style="background-color:#82e9ff; border-radius: 15px;"><svg style="width:24px;height:24px" viewBox="0 0 24 24">     <path fill="#000000" d="M11,4H13V16L18.5,10.5L19.92,11.92L12,19.84L4.08,11.92L5.5,10.5L11,16V4Z" /> </svg><svg style="width:24px;height:24px" viewBox="0 0 24 24">     <path fill="#000000" d="M11,4H13V16L18.5,10.5L19.92,11.92L12,19.84L4.08,11.92L5.5,10.5L11,16V4Z" /> </svg><svg style="width:24px;height:24px" viewBox="0 0 24 24">     <path fill="#000000" d="M11,4H13V16L18.5,10.5L19.92,11.92L12,19.84L4.08,11.92L5.5,10.5L11,16V4Z" /> </svg><svg style="width:24px;height:24px" viewBox="0 0 24 24">     <path fill="#000000" d="M11,4H13V16L18.5,10.5L19.92,11.92L12,19.84L4.08,11.92L5.5,10.5L11,16V4Z" /> </svg><svg style="width:24px;height:24px" viewBox="0 0 24 24">     <path fill="#000000" d="M11,4H13V16L18.5,10.5L19.92,11.92L12,19.84L4.08,11.92L5.5,10.5L11,16V4Z" /> </svg></div></td></tr>
-						<tr><td style="border-bottom: none; vertical-align:center;">Zpráva:<br><input id="text" name="zprava" style="width:99%; height:20px;" autofocus>';
+						<tr><td style="border-bottom: none; vertical-align:center;">Zpráva:<br><input id="text" onKeyDown="if(event.keyCode==13) submit();" name="zprava" style="width:99%; height:20px;" autofocus>';
 						/*if(isset($_GET["zprava"])) {echo $_GET["zprava"];}*/
 						echo '</td></tr>
 						<tr><td style="border-top: none; text-align:center;">Čas: <input type="text" name="cas" value="10';
@@ -103,9 +103,9 @@ if (!isset($nazev)) {
 						<div style="text-align:center; display:none;" id="upravit">
 							<div style="display:inline-block; text-align:left; width:95%; height:90%;">
 								<table style="width:100%; border-collapse: collapse; text-align:left; height:100%; background-color:white; color:black;">
-									<tr><td><div onclick="zpetupr()"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
-    <path fill="#555555" d="M21,11H6.83L10.41,7.41L9,6L3,12L9,18L10.41,16.58L6.83,13H21V11Z" />
-</svg><div style="display:inline; color:#555555;">Zpět</div></div><br></td></tr>
+									<tr><td><div id="zrusit" onclick="zpetupr()"><svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="#555555" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+</svg><div style="display:inline; color:#555555;"> Zrušit</div></div><br></td></tr>
 									<tr><td><div id="divupravit"></div></td></tr>
 								</table>
 							</div>
@@ -113,11 +113,15 @@ if (!isset($nazev)) {
 <script>
 function editvar(typ, id, hodnota) {
 	if (typ != "" && id != "" && hodnota != "") {
-		document.getElementById("editsubmittext").style.display = "none";
+		document.getElementById("editsubmittext").style.display = "inline";
+		document.getElementById("editsubmittext").innerHTML = "Úpravy se odesílají na server.";
+		document.getElementById("zrusit").style.display = "none";
 	var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("editsubmitbtn").style.display = "inline";
+		document.getElementById("editsubmittext").style.display = "none";
+		document.getElementById("editsubmitbtn").style.display = "inline";
+		document.getElementById("editsubmitbtn").focus();
     }
   };
   xhttp.open("GET", "rodeslat.php?id='.$id.'&hrac="+id+"&typ="+typ+"&hodnota="+hodnota+"&edit", true);
@@ -153,6 +157,12 @@ function submit() {
   var message = document.getElementsByName("zprava")[0].value;
   if (!isNaN(time) && time != "" && time < 25) {
 	  if (message != "") {
+		document.getElementById("timetick").style.display = "none";
+		document.getElementById("tick").style.display = "none";
+		document.getElementById("odstavec").innerHTML =  "<br>";
+		document.getElementById("submitwrites").innerHTML =  "Odesílání...";
+		document.getElementById("submitchyba").innerHTML = "";
+		document.getElementsByName("zprava")[0].value = "";
 		  var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
@@ -163,10 +173,9 @@ function submit() {
 		if (minuty < 10) {minuty = "0" + minuty;}
 		document.getElementById("timetick").style.display = "none";
 		document.getElementById("tick").style.display = "inline";
-		document.getElementById("odstavec").innerHTML =  "<br>";
 		document.getElementById("submitwrites").innerHTML =  /*" \"" + escapeHtml(message) + "\" – " + */hodiny + ":" + minuty;
 		document.getElementById("submitchyba").innerHTML = "";
-		document.getElementsByName("zprava")[0].value = "";
+		setTimeout(function(){ document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight;},1001);
     }
   };
   xhttp.open("POST", "rodeslat.php", true);
@@ -174,6 +183,11 @@ function submit() {
   xhttp.send("id='.$id.'&autor=0&cas="+time+"&zprava="+message);
 	  }
 	  else {
+		document.getElementById("timetick").style.display = "none";
+		document.getElementById("tick").style.display = "none";
+		document.getElementById("odstavec").innerHTML =  "<br>";
+		document.getElementById("submitwrites").innerHTML =  "Odesílání času...";
+		document.getElementById("submitchyba").innerHTML = "";
 		  var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
